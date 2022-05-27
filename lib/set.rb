7cc98937632976c7a90789881f5109187b5038db
Jeremy Ward <celloward@gmail.com>
2022-05-27 00:07:56 -0400

    Allow optional title for set and squash bug for single-tune sets rendering

--- lib/set.rb
+++ lib/set.rb
@@ -7,16 +7,29 @@ class Set
     @last_page = last_page
   end
 
+  # Only want to have a order and title if a title exists. Otherwise just the tune and new page
   def template
-    @template = <<~TEMPLATE
-      #{ordering}
-      #{titling}
+    @template = <<~TEMPLATE.prepend(frontmatter)
       #{tunes}
       #{endmatter}
     TEMPLATE
   end
 
+  # Don't return frontmatter when no explicit set title
+  def frontmatter
+    return "" unless set.title
+
+    <<~FRONTMATTER
+      #{ordering}
+      #{titling}
+    FRONTMATTER
+  end
+
+  # Don't want extra X factors when only 1 tune is in set
+  # It causes ABC not to interpret the tune which itself has an X factor
   def ordering
+    return "" unless set.tunes.count > 1
+
     "X: #{set.order}"
   end
 
